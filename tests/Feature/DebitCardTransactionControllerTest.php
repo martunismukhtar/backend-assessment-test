254b7f66260415fc8be6946d5be919800588ed68
Martunis <martunismukhtar@gmail.com>
2025-11-22 06:19:44 +0700

    testCustomerCannotSeeAListOfDebitCardTransactionsOfOtherCustomerDebitCard

--- tests/Feature/DebitCardTransactionControllerTest.php
+++ tests/Feature/DebitCardTransactionControllerTest.php
@@ -49,6 +49,17 @@ class DebitCardTransactionControllerTest extends TestCase
     public function testCustomerCannotSeeAListOfDebitCardTransactionsOfOtherCustomerDebitCard()
     {
         // get /debit-card-transactions
+        $card = DebitCard::factory()->create();
+
+        DebitCardTransaction::factory()->create([
+            'debit_card_id' => $card->id,
+            'amount'        => 100000,
+            'currency_code' => 'IDR',
+        ]);
+
+        $response = $this->getJson("/api/debit-card-transactions?debit_card_id={$card->id}");
+
+        $response->assertStatus(403);
     }
 
     public function testCustomerCanCreateADebitCardTransaction()
