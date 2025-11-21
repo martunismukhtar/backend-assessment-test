fc420c3313d671e5930f6c3e926c724fc8936d2d
Martunis <martunismukhtar@gmail.com>
2025-11-22 06:13:52 +0700

    testCustomerCanSeeAListOfDebitCardTransactions

--- tests/Feature/DebitCardTransactionControllerTest.php
+++ tests/Feature/DebitCardTransactionControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\DebitCard;
+use App\Models\DebitCardTransaction;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Passport\Passport;
@@ -28,7 +29,22 @@ class DebitCardTransactionControllerTest extends TestCase
     public function testCustomerCanSeeAListOfDebitCardTransactions()
     {
         // get /debit-card-transactions
+        $card = DebitCard::factory()->create([
+            'user_id' => $this->user->id,
+        ]);
+
+        DebitCardTransaction::factory()->create([
+            'debit_card_id' => $card->id,
+            'amount' => 100000,
+            'currency_code' => 'IDR',
+        ]);
+        $debit_card_id = $card->id;
+        $response = $this->getJson("/api/debit-card-transactions?debit_card_id={$debit_card_id}");
+
+        $response->assertStatus(200);
+
     }
+
 
     public function testCustomerCannotSeeAListOfDebitCardTransactionsOfOtherCustomerDebitCard()
     {
